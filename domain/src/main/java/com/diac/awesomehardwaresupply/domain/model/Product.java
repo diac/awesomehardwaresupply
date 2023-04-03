92299650bdea0cd3460cb388f39479e1e8a01c2d
diac <nikolai.gladkikh@gmail.com>
2023-04-03 14:28:24 +0300

    [#184854433] Knowledgebase: Convert Product to a JPA entity

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/Product.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/Product.java
@@ -1,10 +1,13 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 /**
  * Модель данных "Товар"
  */
+@Entity
+@Table(name = "product")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,6 +18,8 @@ public class Product {
     /**
      * Идентификатор товара
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
