a750595e82f0bd4902ec5ae03a90363593469741
diac <nikolai.gladkikh@gmail.com>
2023-04-04 10:35:16 +0300

    [#184858969] Knowledgebase: Add validation for the Product fields

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/Product.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/Product.java
@@ -1,6 +1,8 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
@@ -26,15 +28,21 @@ public class Product {
     /**
      * Наименование товара
      */
+    @NotNull(message = "Product name is required")
+    @NotBlank(message = "Product name cannot be blank")
     private String name;
 
     /**
      * Описание товара
      */
+    @NotNull(message = "Product description is required")
+    @NotBlank(message = "Product description cannot be blank")
     private String description;
 
     /**
      * Артикул товара
      */
+    @NotNull(message = "Product sku is required")
+    @NotBlank(message = "Product sku cannot be blank")
     private String sku;
 }
