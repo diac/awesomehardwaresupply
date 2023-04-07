6c6c019d594ae1ad1bef463f41be082e6f0b8a1c
diac <nikolai.gladkikh@gmail.com>
2023-04-07 13:09:06 +0300

    [#184866469] Create "authentication" module

---
+++ authentication/src/main/java/com/diac/awesomehardwaresupply/authentication/AuthenticationApp.java
@@ -0,0 +1,14 @@
+package com.diac.awesomehardwaresupply.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthenticationApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationApp.class, args);
+    }
+}
