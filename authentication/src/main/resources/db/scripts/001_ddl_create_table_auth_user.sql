af11bb530f40697e75159d96bfe393e65c505aec
diac <nikolai.gladkikh@gmail.com>
2023-04-07 13:32:35 +0300

    [#184866469] Authentication module: Create "auth_user" DB table

---
+++ authentication/src/main/resources/db/scripts/001_ddl_create_table_auth_user.sql
@@ -0,0 +1,12 @@
+CREATE TABLE auth_user (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    email VARCHAR UNIQUE NOT NULL
+);
+
+COMMENT ON TABLE auth_user IS 'Пользователь';
+COMMENT ON COLUMN auth_user.id IS 'Идентификатор пользователя';
+COMMENT ON COLUMN auth_user.username IS 'Имя пользователя';
+COMMENT ON COLUMN auth_user.password IS 'Пароль пользоватея';
+COMMENT ON COLUMN auth_user.email IS 'Email пользоватея';
