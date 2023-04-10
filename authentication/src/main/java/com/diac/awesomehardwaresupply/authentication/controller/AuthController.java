f51576add86d80ac4cb88a432547dc1e5902d43a
diac <nikolai.gladkikh@gmail.com>
2023-04-10 18:12:10 +0300

    [#184866469] Authentication module: Add "/auth" request mapping in AuthController

--- authentication/src/main/java/com/diac/awesomehardwaresupply/authentication/controller/AuthController.java
+++ authentication/src/main/java/com/diac/awesomehardwaresupply/authentication/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
 
     /**
