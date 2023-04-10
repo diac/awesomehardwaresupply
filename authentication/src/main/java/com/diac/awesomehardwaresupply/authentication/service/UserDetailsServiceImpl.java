7180f1a9ed764c939278e1a94a20b3db833b6cdd
diac <nikolai.gladkikh@gmail.com>
2023-04-10 11:24:50 +0300

    [#184866469] Authentication module: Add UserDetailsServiceImpl

---
+++ authentication/src/main/java/com/diac/awesomehardwaresupply/authentication/service/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.diac.awesomehardwaresupply.authentication.service;
+
+import com.diac.awesomehardwaresupply.authentication.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+/**
+ * Сервис для работы с объектами UserDetails
+ */
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    /**
+     * Репозиторий для хранения объектов User
+     */
+    private final UserRepository userRepository;
+
+    /**
+     * Получить объект UserDetails по имени пользователя
+     *
+     * @param username Имя пользователя
+     * @return Объект UserDetails для переданного имени пользователя
+     * @throws UsernameNotFoundException В случае, если пользователь с переданным на найден
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(
+                        user -> new User(user.getUsername(), String.valueOf(user.getPassword()), Collections.emptyList())
+                ).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
