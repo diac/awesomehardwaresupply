f85a92aa3bd21211a7520e8a303c0157200639e8
diac <nikolai.gladkikh@gmail.com>
2023-04-10 13:34:06 +0300

    [#184866469] Authentication module: Add signIn() and validateToken() methods in AuthController

--- authentication/src/main/java/com/diac/awesomehardwaresupply/authentication/controller/AuthController.java
+++ authentication/src/main/java/com/diac/awesomehardwaresupply/authentication/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.diac.awesomehardwaresupply.authentication.controller;
 
+import com.diac.awesomehardwaresupply.authentication.dto.JwtTokenResponseDto;
+import com.diac.awesomehardwaresupply.authentication.dto.JwtTokenValidationDto;
+import com.diac.awesomehardwaresupply.authentication.dto.UserCredentialsDto;
 import com.diac.awesomehardwaresupply.authentication.model.User;
+import com.diac.awesomehardwaresupply.authentication.service.AuthService;
 import com.diac.awesomehardwaresupply.authentication.service.JwtService;
 import com.diac.awesomehardwaresupply.authentication.service.UserService;
 import jakarta.validation.Valid;
@@ -9,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
 public class AuthController {
 
     /**
@@ -36,6 +38,11 @@ public class AuthController {
     private final JwtService jwtService;
 
     /**
+     * Сервис, отвечающий за авторизацию пользователей
+     */
+    private final AuthService authService;
+
+    /**
      * Зарегистрировать нового пользователя
      *
      * @param user Данные пользователя для регистрации
@@ -48,5 +55,30 @@ public class AuthController {
                 String.format(USER_REGISTER_SUCCESS_MESSAGE, newUser.getUsername()),
                 HttpStatus.CREATED
         );
+    }
+
+    /**
+     * Авторизовать пользователя в системе
+     *
+     * @param userCredentialsDto DTO с данными авторизации пользователя
+     * @return Тело ответа с JWT токеном
+     */
+    @PostMapping("/sign-in")
+    public ResponseEntity<JwtTokenResponseDto> signIn(@RequestBody UserCredentialsDto userCredentialsDto) {
+        return new ResponseEntity<>(
+                authService.signIn(userCredentialsDto),
+                HttpStatus.OK
+        );
+    }
+
+    /**
+     * Проверить валидность предоставленного токена
+     *
+     * @param jwtTokenValidationDto DTO с данными токена
+     */
+    @PostMapping("/validate-token")
+    public ResponseEntity<Void> validateToken(@RequestBody @Valid JwtTokenValidationDto jwtTokenValidationDto) {
+        jwtService.validateToken(jwtTokenValidationDto.getToken());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
