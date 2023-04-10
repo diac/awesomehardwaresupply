52be4aaf9652b2322df22b5fb7ecd851a56272f6
diac <nikolai.gladkikh@gmail.com>
2023-04-10 13:00:13 +0300

    [#184866469] Authentication module: Add JwtTokenValidationDto

---
+++ authentication/src/main/java/com/diac/awesomehardwaresupply/authentication/dto/JwtTokenValidationDto.java
@@ -0,0 +1,23 @@
+package com.diac.awesomehardwaresupply.authentication.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO для валидации передаваемого JWT токена
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtTokenValidationDto {
+
+    /**
+     * Значение токена
+     */
+    @NotNull(message = "Token is required")
+    @NotBlank(message = "Token cannot be blank")
+    private String token;
+}
