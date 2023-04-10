997c268b7db0e0abacdfda638229cc4c9e1dbc9c
diac <nikolai.gladkikh@gmail.com>
2023-04-10 11:01:42 +0300

    [#184866469] Authentication module: Add possibility to register new user

---
+++ authentication/src/main/java/com/diac/awesomehardwaresupply/authentication/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.diac.awesomehardwaresupply.authentication.controller;
+
+import com.diac.awesomehardwaresupply.authentication.model.User;
+import com.diac.awesomehardwaresupply.authentication.service.JwtService;
+import com.diac.awesomehardwaresupply.authentication.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Контроллер, ответственный за авторизацию пользователей
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    /**
+     * Шаблон сообщения об успешной регистрации пользователя
+     */
+    private static final String USER_REGISTER_SUCCESS_MESSAGE = "User %s has been successfully registered";
+
+    /**
+     * Сервис для работы с объектами модели User
+     */
+    private final UserService userService;
+
+    /**
+     * Сервис для работы с JWT
+     */
+    private final JwtService jwtService;
+
+    /**
+     * Зарегистрировать нового пользователя
+     *
+     * @param user Данные пользователя для регистрации
+     * @return Тело ответа со статусом и сообщением об успешной регистрации
+     */
+    public ResponseEntity<String> register(@RequestBody @Valid User user) {
+        User newUser = userService.addUser(user);
+        return new ResponseEntity<>(
+                String.format(USER_REGISTER_SUCCESS_MESSAGE, newUser.getUsername()),
+                HttpStatus.CREATED
+        );
+    }
+}
