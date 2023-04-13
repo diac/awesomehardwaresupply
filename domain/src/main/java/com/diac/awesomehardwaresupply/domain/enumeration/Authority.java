0488aa05781cb5c3d03501a6c57b9574ff8cefce
diac <nikolai.gladkikh@gmail.com>
2023-04-13 10:13:10 +0300

    [#184876300] Add Authority enumeration

---
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/enumeration/Authority.java
@@ -0,0 +1,17 @@
+package com.diac.awesomehardwaresupply.domain.enumeration;
+
+/**
+ * Перечисление привилегий пользователя в системе
+ */
+public enum Authority {
+
+    /**
+     * Чтение товаров
+     */
+    READ_PRODUCTS,
+
+    /**
+     * Изменение товаров
+     */
+    WRITE_PRODUCTS
+}
