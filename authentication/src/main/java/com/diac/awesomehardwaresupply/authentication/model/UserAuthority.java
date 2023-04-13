b5ddc0cb5c50c09339d333d009d451300888616e
diac <nikolai.gladkikh@gmail.com>
2023-04-13 10:48:30 +0300

    [#184876300] Authentication module: Add UserAuthority model

---
+++ authentication/src/main/java/com/diac/awesomehardwaresupply/authentication/model/UserAuthority.java
@@ -0,0 +1,31 @@
+package com.diac.awesomehardwaresupply.authentication.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Модель данных "Привилегия пользователя"
+ */
+@Entity
+@Table(name = "user_authority")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+public class UserAuthority {
+
+    /**
+     * Идентификатор привилегии
+     */
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Include
+    private int id;
+
+    /**
+     * Значение привилегии
+     */
+    @Enumerated(EnumType.STRING)
+    private String authority;
+}
