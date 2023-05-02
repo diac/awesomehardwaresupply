18065922a12e31676bb003ab75c0b31a18860f1b
diac <nikolai.gladkikh@gmail.com>
2023-05-02 12:27:46 +0300

    [#185078661] Knowledgebase: Add tests for ProductCategoryRepository

---
+++ knowledgebase/src/test/java/com/diac/awesomehardwaresupply/knowledgebase/repository/ProductCategoryRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.diac.awesomehardwaresupply.knowledgebase.repository;
+
+import com.diac.awesomehardwaresupply.domain.model.ProductCategory;
+import com.diac.awesomehardwaresupply.knowledgebase.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@ContextConfiguration(classes = {
+        DataConfig.class,
+        ProductCategory.class
+})
+public class ProductCategoryRepositoryTest {
+
+    @Autowired
+    private ProductCategoryRepository productCategoryRepository;
+
+    @Test
+    public void whenFindAll() {
+        String value = "test";
+        ProductCategory productCategory = productCategoryRepository.save(
+                ProductCategory.builder()
+                        .name(value)
+                        .description(value)
+                        .build()
+        );
+        assertThat(productCategoryRepository.findAll()).contains(productCategory);
+    }
+
+    @Test
+    public void whenFindById() {
+        String value = "test";
+        ProductCategory productCategory = productCategoryRepository.save(
+                ProductCategory.builder()
+                        .name(value)
+                        .description(value)
+                        .build()
+        );
+        ProductCategory productCategoryInDb = productCategoryRepository.findById(productCategory.getId())
+                .orElse(new ProductCategory());
+        assertThat(productCategoryInDb).isEqualTo(productCategory);
+    }
+
+    @Test
+    public void whenAdd() {
+        String value = "test";
+        ProductCategory productCategory = productCategoryRepository.save(
+                ProductCategory.builder()
+                        .name(value)
+                        .description(value)
+                        .build()
+        );
+        assertThat(productCategory.getName()).isEqualTo(value);
+        assertThat(productCategory.getDescription()).isEqualTo(value);
+    }
+
+    @Test
+    public void whenUpdate() {
+        String value = "test";
+        String updatedValue = value + "_updated";
+        ProductCategory productCategory = productCategoryRepository.save(
+                ProductCategory.builder()
+                        .name(value)
+                        .description(value)
+                        .build()
+        );
+        productCategory.setName(updatedValue);
+        productCategory.setDescription(updatedValue);
+        ProductCategory updatedProductCategory = productCategoryRepository.save(productCategory);
+        assertThat(productCategory).isEqualTo(updatedProductCategory);
+        assertThat(productCategory.getName()).isEqualTo(updatedProductCategory.getName());
+        assertThat(productCategory.getDescription()).isEqualTo(updatedProductCategory.getDescription());
+    }
+
+    @Test
+    public void whenDelete() {
+        String value = "test";
+        ProductCategory productCategory = productCategoryRepository.save(
+                ProductCategory.builder()
+                        .name(value)
+                        .description(value)
+                        .build()
+        );
+        productCategoryRepository.delete(productCategory);
+        assertThat(productCategoryRepository.findAll()).doesNotContain(productCategory);
+    }
+}
