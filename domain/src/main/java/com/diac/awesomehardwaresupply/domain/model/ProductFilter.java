93675c293e317e1f20b57d88d1512cf4b6df9d56
diac <nikolai.gladkikh@gmail.com>
2023-05-03 17:01:23 +0300

    [#185078684] Knowledgebase: Convert the ProductFilter model to a JPA entity

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductFilter.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductFilter.java
@@ -1,10 +1,15 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
  * Модель данных "Фильтр товаров"
  */
+@Entity
+@Table(name = "product_filter")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,16 +20,22 @@ public class ProductFilter {
     /**
      * Идентификатор фильтра товаров
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
     /**
      * Наименование фильтра товаров
      */
+    @NotNull(message = "Product filter name is required")
+    @NotBlank(message = "Product filter name cannot be blank")
     private String name;
 
     /**
      * Описание фильтра товаров
      */
+    @NotNull(message = "Product filter description is required")
+    @NotBlank(message = "Product filter description cannot be blank")
     private String description;
 }
