787a68b119f2698a3f6886fd73ebf0afa465f04f
diac <nikolai.gladkikh@gmail.com>
2023-05-03 17:05:15 +0300

    [#185078684] Knowledgebase: Convert the ProductTag model to a JPA entity

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductTag.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductTag.java
@@ -1,10 +1,15 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
  * Модель данных "Тэг товаров"
  */
+@Entity
+@Table(name = "product_tag")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,21 +20,28 @@ public class ProductTag {
     /**
      * Идентификатор тэга
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
     /**
      * Наименование тэга
      */
+    @NotNull(message = "Tag name is required")
+    @NotBlank(message = "Tag name cannot be blank")
     private String name;
 
     /**
      * Описание тэга
      */
+    @NotNull(message = "Tag description is required")
+    @NotBlank(message = "Tag description cannot be blank")
     private String description;
 
     /**
      * Фильтр, к которому принадлежит тэг
      */
+    @NotNull(message = "Associated Product Filter is required")
     private ProductFilter productFilter;
 }
