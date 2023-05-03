39119f5b3d2e4db4e93fc83e89c133ab79ba1ff7
diac <nikolai.gladkikh@gmail.com>
2023-05-03 17:35:42 +0300

    [#185078684] Knowledgebase: Convert the ProductSpecificationValue model to a JPA entity

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductSpecificationValueRange.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductSpecificationValueRange.java
@@ -1,10 +1,14 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
  * Модель данных "Диапазон значений спецификации товара"
  */
+@Entity
+@Table(name = "product_specification_value_range")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,21 +19,30 @@ public class ProductSpecificationValueRange {
     /**
      * Идентификатор диапазона значений
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
     /**
      * Минимальная величина значения
      */
-    private float valueMin;
+    @Column(name = "value_min")
+    @NotNull(message = "Value minimum is required")
+    private Float valueMin;
 
     /**
      * Максимальная величина значения
      */
-    private float valueMax;
+    @Column(name = "value_max")
+    @NotNull(message = "Value maximum is required")
+    private Float valueMax;
 
     /**
      * Спецификация, которой принадлежит значение
      */
+    @ManyToOne
+    @JoinColumn(name = "product_specification_id")
+    @NotNull(message = "Associated Product Specification is required")
     private ProductSpecification productSpecification;
 }
