2b6a76a0a873e33de5c2af238b0facb7c77a9694
diac <nikolai.gladkikh@gmail.com>
2023-05-03 17:14:01 +0300

    [#185078684] Knowledgebase: Convert the ProductSpecification model to a JPA entity

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductSpecification.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductSpecification.java
@@ -1,10 +1,15 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
  * Модель данных "Спецификация товаров"
  */
+@Entity
+@Table(name = "product_specification")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,21 +20,29 @@ public class ProductSpecification {
     /**
      * Идентификатор спецификации товаров
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
     /**
      * Наименование спецификации
      */
+    @NotNull(message = "Specification name is required")
+    @NotBlank(message = "Specification name cannot be blank")
     private String name;
 
     /**
      * Описание спецификации
      */
+    @NotNull(message = "Specification description is required")
+    @NotBlank(message = "Specification description cannot be blank")
     private String description;
 
     /**
      * Единица измерения
      */
+    @NotNull(message = "Specification units is required")
+    @NotBlank(message = "Specification units cannot be blank")
     private String units;
 }
