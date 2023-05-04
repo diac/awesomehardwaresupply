179ae6741c96ffc9233c487b8122581f4e601efa
diac <nikolai.gladkikh@gmail.com>
2023-05-04 13:26:26 +0300

    [#185078684] Knowledgebase: Add tests for ProductSpecificationRepository

---
+++ knowledgebase/src/test/java/com/diac/awesomehardwaresupply/knowledgebase/repository/ProductSpecificationRepositoryTest.java
@@ -0,0 +1,100 @@
+package com.diac.awesomehardwaresupply.knowledgebase.repository;
+
+import com.diac.awesomehardwaresupply.domain.model.ProductSpecification;
+import com.diac.awesomehardwaresupply.knowledgebase.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ContextConfiguration(classes = {
+        DataConfig.class,
+        ProductSpecificationRepository.class
+})
+public class ProductSpecificationRepositoryTest {
+
+    @Autowired
+    private ProductSpecificationRepository productSpecificationRepository;
+
+    @Test
+    public void whenFindAll() {
+        String value = "test";
+        ProductSpecification productSpecification = productSpecificationRepository.save(
+                ProductSpecification.builder()
+                        .name(value)
+                        .description(value)
+                        .units(value)
+                        .build()
+        );
+        assertThat(productSpecificationRepository.findAll()).contains(productSpecification);
+    }
+
+    @Test
+    public void whenFindById() {
+        String value = "test";
+        ProductSpecification productSpecification = productSpecificationRepository.save(
+                ProductSpecification.builder()
+                        .name(value)
+                        .description(value)
+                        .units(value)
+                        .build()
+        );
+        ProductSpecification productSpecificationInDb
+                = productSpecificationRepository.findById(productSpecification.getId())
+                .orElse(new ProductSpecification());
+        assertThat(productSpecificationInDb).isEqualTo(productSpecification);
+    }
+
+    @Test
+    public void whenAdd() {
+        String value = "test";
+        ProductSpecification productSpecification = productSpecificationRepository.save(
+                ProductSpecification.builder()
+                        .name(value)
+                        .description(value)
+                        .units(value)
+                        .build()
+        );
+        assertThat(productSpecification.getName()).isEqualTo(value);
+        assertThat(productSpecification.getDescription()).isEqualTo(value);
+        assertThat(productSpecification.getUnits()).isEqualTo(value);
+    }
+
+    @Test
+    public void whenUpdate() {
+        String value = "test";
+        String updatedValue = value + "_updated";
+        ProductSpecification productSpecification = productSpecificationRepository.save(
+                ProductSpecification.builder()
+                        .name(value)
+                        .description(value)
+                        .units(value)
+                        .build()
+        );
+        productSpecification.setName(updatedValue);
+        productSpecification.setDescription(updatedValue);
+        productSpecification.setUnits(updatedValue);
+        ProductSpecification updatedProductSpecification = productSpecificationRepository.save(productSpecification);
+        assertThat(productSpecification).isEqualTo(updatedProductSpecification);
+        assertThat(productSpecification.getName()).isEqualTo(updatedProductSpecification.getName());
+        assertThat(productSpecification.getDescription()).isEqualTo(updatedProductSpecification.getDescription());
+        assertThat(productSpecification.getUnits()).isEqualTo(updatedProductSpecification.getDescription());
+    }
+
+    @Test
+    public void whenDelete() {
+        String value = "test";
+        ProductSpecification productSpecification = productSpecificationRepository.save(
+                ProductSpecification.builder()
+                        .name(value)
+                        .description(value)
+                        .units(value)
+                        .build()
+        );
+        productSpecificationRepository.delete(productSpecification);
+        assertThat(productSpecificationRepository.findAll()).doesNotContain(productSpecification);
+    }
+}
