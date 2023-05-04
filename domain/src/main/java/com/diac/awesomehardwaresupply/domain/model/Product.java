7e3382d6d6cf246eee775dc59a796308d689cdd4
diac <nikolai.gladkikh@gmail.com>
2023-05-04 11:52:48 +0300

    [#185078684] Knowledgebase: Add many-to-many relations to Product

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/Product.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/Product.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
+import java.util.List;
+
 /**
  * Модель данных "Товар"
  */
@@ -53,4 +55,37 @@ public class Product {
     @JoinColumn(name = "product_category_id")
     @NotNull(message = "Product Category is required")
     private ProductCategory productCategory;
+
+    /**
+     * Тэги товара
+     */
+    @ManyToMany
+    @JoinTable(
+            name = "product_product_tag",
+            joinColumns = {@JoinColumn(name = "product_id")},
+            inverseJoinColumns = {@JoinColumn(name = "product_tag_id")}
+    )
+    private List<ProductTag> productTags;
+
+    /**
+     * Значения спецификаций товара
+     */
+    @ManyToMany
+    @JoinTable(
+            name = "product_product_specification_value",
+            joinColumns = {@JoinColumn(name = "product_id")},
+            inverseJoinColumns = {@JoinColumn(name = "product_specification_value_id")}
+    )
+    private List<ProductSpecificationValue> productSpecificationValues;
+
+    /**
+     * Значения диапазонов спецификаций товара
+     */
+    @ManyToMany
+    @JoinTable(
+            name = "product_product_specification_value_range",
+            joinColumns = {@JoinColumn(name = "product_id")},
+            inverseJoinColumns = {@JoinColumn(name = "product_specification_value_range_id")}
+    )
+    private List<ProductSpecificationValueRange> productSpecificationValueRanges;
 }
