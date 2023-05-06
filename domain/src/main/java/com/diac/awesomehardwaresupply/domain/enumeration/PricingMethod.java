b3242f822a9252c5ac58d72a2bc9adc4f8076957
diac <nikolai.gladkikh@gmail.com>
2023-05-06 13:05:25 +0300

    [#185118636] Price Schedule: Add PricingMethod enumeration

---
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/enumeration/PricingMethod.java
@@ -0,0 +1,32 @@
+package com.diac.awesomehardwaresupply.domain.enumeration;
+
+/**
+ * Перечисление методов ценообразования
+ */
+public enum PricingMethod {
+
+    /**
+     * Абсолютная величина наценки от стоимости
+     */
+    COST_MARKUP_AMOUNT,
+
+    /**
+     * Процентная величина наценки от стоимости
+     */
+    COST_MARKUP_PERCENTAGE,
+
+    /**
+     * Абсолютная величина скидки от цены
+     */
+    PRICE_DISCOUNT_AMOUNT,
+
+    /**
+     * Процентная величина скидки от цены
+     */
+    PRICE_DISCOUNT_PERCENTAGE,
+
+    /**
+     * Переопределение цены
+     */
+    PRICE_OVERRIDE
+}
