59c9d96ebca6a465f928a1b6fda0b5db5b9c4392
diac <nikolai.gladkikh@gmail.com>
2023-05-08 12:20:41 +0300

    [#184854679] Knowledgebase: Add pricing columns to the "product" DB table

---
+++ knowledgebase/src/main/resources/db/scripts/012_ddl_alter_table_product_add_pricing_columns.sql
@@ -0,0 +1,18 @@
+ALTER TABLE
+    product
+ADD COLUMN
+    list_price INTEGER NOT NULL;
+
+ALTER TABLE
+    product
+ADD COLUMN
+    cost INTEGER NOT NULL;
+
+ALTER TABLE
+    product
+ADD COLUMN
+    price_code INTEGER NOT NULL;
+
+COMMENT ON COLUMN product.list_price IS 'Цена товара по прейскуранту';
+COMMENT ON COLUMN product.cost IS 'Стоимость товара';
+COMMENT ON COLUMN product.price_code IS 'Код товара';
