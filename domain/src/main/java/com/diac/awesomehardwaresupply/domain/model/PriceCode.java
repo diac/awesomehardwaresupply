5df41d714916483c41b5641f72be2266a6e3137f
diac <nikolai.gladkikh@gmail.com>
2023-05-10 10:35:54 +0300

    [#185118638] Price Schedule: Convert PriceCode model to a JPA entity

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/PriceCode.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/PriceCode.java
@@ -1,10 +1,15 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
  * Модель данных "Код цены"
  */
+@Entity
+@Table(name = "price_code")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,11 +20,15 @@ public class PriceCode {
     /**
      * Идентификатор кода цены
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
     /**
      * Имя кода цены
      */
+    @NotNull(message = "Price code name is required")
+    @NotBlank(message = "Price code name cannot be blank")
     private String name;
 }
