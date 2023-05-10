b88d5dc0253cb75ff5a0329976b63064c815792c
diac <nikolai.gladkikh@gmail.com>
2023-05-10 11:01:20 +0300

    [#185118638] Price Schedule: Convert PriceCodePricing model to a JPA entity

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/PriceCodePricing.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/PriceCodePricing.java
@@ -1,5 +1,7 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -7,6 +9,8 @@ import java.util.List;
 /**
  * Модель данных "Правило ценообразования по коду цены"
  */
+@Entity
+@Table(name = "price_code_pricing")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,21 +21,35 @@ public class PriceCodePricing {
     /**
      * Идентификатор правила
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
     /**
      * Уровень цены
      */
+    @ManyToOne
+    @JoinColumn(name = "price_level_id")
+    @NotNull(message = "Price level is required")
     private PriceLevel priceLevel;
 
     /**
      * Код цены
      */
+    @ManyToOne
+    @JoinColumn(name = "price_code_id")
+    @NotNull(message = "Price code is required")
     private PriceCode priceCode;
 
     /**
      * Шаги ценообразования
      */
+    @ManyToMany
+    @JoinTable(
+            name = "price_code_pricing_pricing_step",
+            joinColumns = {@JoinColumn(name = "price_code_pricing_id")},
+            inverseJoinColumns = {@JoinColumn(name = "pricing_step_id")}
+    )
     List<PricingStep> pricingSteps;
 }
