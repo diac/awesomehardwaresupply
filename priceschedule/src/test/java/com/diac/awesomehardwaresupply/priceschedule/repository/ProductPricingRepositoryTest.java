87dc64e00e6c4c918b1da94f3d8c693fb9e6eb9e
diac <nikolai.gladkikh@gmail.com>
2023-05-10 15:46:15 +0300

    [#185118639] Price Schedule: Add PricingStep lists to ProductPricing builder calls in ProductPricingRepositoryTest

--- priceschedule/src/test/java/com/diac/awesomehardwaresupply/priceschedule/repository/ProductPricingRepositoryTest.java
+++ priceschedule/src/test/java/com/diac/awesomehardwaresupply/priceschedule/repository/ProductPricingRepositoryTest.java
@@ -1,12 +1,17 @@
 package com.diac.awesomehardwaresupply.priceschedule.repository;
 
+import com.diac.awesomehardwaresupply.domain.enumeration.PricingMethod;
 import com.diac.awesomehardwaresupply.domain.model.PriceLevel;
+import com.diac.awesomehardwaresupply.domain.model.PricingStep;
 import com.diac.awesomehardwaresupply.domain.model.ProductPricing;
 import com.diac.awesomehardwaresupply.priceschedule.config.DataConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,6 +40,16 @@ public class ProductPricingRepositoryTest {
                                                 .build()
                                 )
                         )
+                        .pricingSteps(
+                                List.of(
+                                        PricingStep.builder()
+                                                .priceAdjustment(1000)
+                                                .maxQuantity(1000)
+                                                .minQuantity(1000)
+                                                .pricingMethod(PricingMethod.PRICE_OVERRIDE)
+                                                .build()
+                                )
+                        )
                         .build()
         );
         assertThat(productPricingRepository.findAll()).contains(productPricing);
@@ -50,6 +65,16 @@ public class ProductPricingRepositoryTest {
                                 priceLevelRepository.save(
                                         PriceLevel.builder()
                                                 .name(value)
+                                                .build()
+                                )
+                        )
+                        .pricingSteps(
+                                List.of(
+                                        PricingStep.builder()
+                                                .priceAdjustment(1000)
+                                                .maxQuantity(1000)
+                                                .minQuantity(1000)
+                                                .pricingMethod(PricingMethod.PRICE_OVERRIDE)
                                                 .build()
                                 )
                         )
@@ -72,6 +97,16 @@ public class ProductPricingRepositoryTest {
                 ProductPricing.builder()
                         .productSku(value)
                         .priceLevel(priceLevel)
+                        .pricingSteps(
+                                List.of(
+                                        PricingStep.builder()
+                                                .priceAdjustment(1000)
+                                                .maxQuantity(1000)
+                                                .minQuantity(1000)
+                                                .pricingMethod(PricingMethod.PRICE_OVERRIDE)
+                                                .build()
+                                )
+                        )
                         .build()
         );
         assertThat(productPricing.getProductSku()).isEqualTo(value);
@@ -91,6 +126,16 @@ public class ProductPricingRepositoryTest {
                 ProductPricing.builder()
                         .productSku(value)
                         .priceLevel(priceLevel)
+                        .pricingSteps(
+                                List.of(
+                                        PricingStep.builder()
+                                                .priceAdjustment(1000)
+                                                .maxQuantity(1000)
+                                                .minQuantity(1000)
+                                                .pricingMethod(PricingMethod.PRICE_OVERRIDE)
+                                                .build()
+                                )
+                        )
                         .build()
         );
         PriceLevel anotherPriceLevel = priceLevelRepository.save(
@@ -100,6 +145,7 @@ public class ProductPricingRepositoryTest {
         );
         productPricing.setProductSku(anotherValue);
         productPricing.setPriceLevel(anotherPriceLevel);
+        productPricing.setPricingSteps(Collections.emptyList());
         ProductPricing updatedProductPricing = productPricingRepository.save(productPricing);
         assertThat(productPricing).isEqualTo(updatedProductPricing);
         assertThat(productPricing.getProductSku()).isEqualTo(updatedProductPricing.getProductSku());
