e78e1567e92defeb5e9896ba0a9b53a1febf20fe
diac <nikolai.gladkikh@gmail.com>
2023-05-10 11:15:45 +0300

    [#185118638] Price Schedule: Convert ProductPricing model to a JPA entity

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductPricing.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/ProductPricing.java
@@ -1,5 +1,8 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -7,6 +10,8 @@ import java.util.List;
 /**
  * Модель данных "Правило ценообразования товара"
  */
+@Entity
+@Table(name = "product_pricing")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,21 +22,35 @@ public class ProductPricing {
     /**
      * Идентификатор правила
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
     /**
      * Артикул товара
      */
+    @Column(name = "product_sku")
+    @NotNull(message = "Product SKU is required")
+    @NotBlank(message = "Product SKU cannot be blank")
     private String productSku;
 
     /**
      * Уровень ценообразования
      */
+    @ManyToOne
+    @JoinColumn(name = "price_level_id")
+    @NotNull(message = "Price level is required")
     private PriceLevel priceLevel;
 
     /**
      * Шаги ценообразования
      */
+    @ManyToOne
+    @JoinTable(
+            name = "product_pricing_pricing_step",
+            joinColumns = {@JoinColumn(name = "product_pricing_id")},
+            inverseJoinColumns = {@JoinColumn(name = "pricing_step_id")}
+    )
     private List<PricingStep> pricingSteps;
 }
