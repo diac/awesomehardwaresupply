0407f69be8c335a04433c0ff26be55779e3313d8
diac <nikolai.gladkikh@gmail.com>
2023-05-10 10:38:43 +0300

    [#185118638] Price Schedule: Convert PriceLevel model to a JPA entity

--- domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/PriceLevel.java
+++ domain/src/main/java/com/diac/awesomehardwaresupply/domain/model/PriceLevel.java
@@ -1,10 +1,15 @@
 package com.diac.awesomehardwaresupply.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
  * Модель данных "Уровень цены"
  */
+@Entity
+@Table(name = "price_level")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,11 +20,15 @@ public class PriceLevel {
     /**
      * Идентификатор уровня цены
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
     /**
      * Имя уровня цены
      */
+    @NotNull(message = "Price level name is required")
+    @NotBlank(message = "Price level name cannot be blank")
     private String name;
 }
