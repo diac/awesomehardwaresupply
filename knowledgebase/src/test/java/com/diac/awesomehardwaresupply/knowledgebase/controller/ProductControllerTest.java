4c2f9dee3e309e26957150e87e27b255ebcf14e9
diac <nikolai.gladkikh@gmail.com>
2023-05-12 12:04:54 +0300

    Fix errors in ProductControllerTest

--- knowledgebase/src/test/java/com/diac/awesomehardwaresupply/knowledgebase/controller/ProductControllerTest.java
+++ knowledgebase/src/test/java/com/diac/awesomehardwaresupply/knowledgebase/controller/ProductControllerTest.java
@@ -101,9 +101,6 @@ public class ProductControllerTest {
                 .description(value)
                 .sku(value)
                 .productCategory(ProductCategory.builder().id(1).build())
-                .listPrice(1000)
-                .cost(1000)
-                .priceCode(value)
                 .build();
         Product savedProduct = Product.builder()
                 .id(id)
@@ -111,9 +108,6 @@ public class ProductControllerTest {
                 .description(value)
                 .sku(value)
                 .productCategory(ProductCategory.builder().id(1).build())
-                .listPrice(1000)
-                .cost(1000)
-                .priceCode(value)
                 .build();
         String requestBody = objectWriter.writeValueAsString(newProduct);
         String responseBody = objectWriter.writeValueAsString(savedProduct);
@@ -167,9 +161,6 @@ public class ProductControllerTest {
                 .description(value)
                 .sku(value)
                 .productCategory(ProductCategory.builder().id(1).build())
-                .listPrice(1000)
-                .cost(1000)
-                .priceCode(value)
                 .build();
         String jsonValue = objectWriter.writeValueAsString(product);
         String requestUrl = String.format("/product/%d", id);
