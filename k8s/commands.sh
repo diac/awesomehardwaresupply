f7eded1859ab270b1efcaa89a7b898c8e062d9dc
diac <nikolai.gladkikh@gmail.com>
2023-05-17 10:52:30 +0300

    [#185097995] Price Schedule K8s configuration: Add K8s commands in commands.sh

--- k8s/commands.sh
+++ k8s/commands.sh
@@ -8,6 +8,9 @@ kubectl apply -f jwt-secret.yml
 # Knowledgebase Secret
 kubectl apply -f knowledgebase-db-secret.yml
 
+# Price Schedule Secret
+kubectl apply -f price-schedule-db-secret.yml
+
 ### Config Maps ###
 # Authentication DB Config Map
 kubectl apply -f authentication-db-configmap.yml
@@ -18,6 +21,9 @@ kubectl apply -f gateway-configmap.yml
 # Knowledgebase Config Map
 kubectl apply -f knowledgebase-db-configmap.yml
 
+# Price Schedule Config Map
+kubectl apply -f price-schedule-db-configmap.yml
+
 ### Deployments ###
 # Authentication Deployment
 kubectl apply -f authentication-deployment.yml
@@ -27,3 +33,6 @@ kubectl apply -f gateway-deployment.yml
 
 # Knowledgebase Deployment
 kubectl apply -f knowledgebase-deployment.yml
+
+# Price Schedule Deployment
+kubectl apply -f price-schedule-deployment.yml
