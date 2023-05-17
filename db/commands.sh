aed5d5aee3721fcb352fc7b74afede4be3deb97f
diac <nikolai.gladkikh@gmail.com>
2023-05-17 13:07:28 +0300

    [#185059531] Correct docker configs for the databases

--- db/commands.sh
+++ db/commands.sh
@@ -1,2 +1,2 @@
-docker build -t postgresql ./
-docker run -itd -p 5432:5432 -v ~/postgres_data:/var/lib/postgresql/data --name postgres postgresql_ahs
+docker build -t postgresql_ahs ./
+docker run -itd -p 5432:5432 -v ~/postgres_data:/var/lib/postgresql/data --name postgresql_ahs postgresql_ahs
