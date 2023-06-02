db359d75464c71c7333f4720be945b5c76af7f19
diac <nikolai.gladkikh@gmail.com>
2023-06-02 18:35:19 +0300

    [#185228206] Knowledgebase: Use Testcontainers PostgreSQL instead of H2 in ProductFilterRepositoryTest

--- knowledgebase/src/test/java/com/diac/awesomehardwaresupply/knowledgebase/repository/ProductFilterRepositoryTest.java
+++ knowledgebase/src/test/java/com/diac/awesomehardwaresupply/knowledgebase/repository/ProductFilterRepositoryTest.java
@@ -4,17 +4,19 @@ import com.diac.awesomehardwaresupply.domain.model.ProductFilter;
 import com.diac.awesomehardwaresupply.knowledgebase.config.DataConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(classes = {
         DataConfig.class,
         ProductFilterRepository.class
 })
-public class ProductFilterRepositoryTest {
+public class ProductFilterRepositoryTest implements PostgreSQLContainerInitializer {
 
     @Autowired
     private ProductFilterRepository productFilterRepository;
