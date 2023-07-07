a925cc224367c69241fe1fca259fe2cc675b449a
diac <nikolai.gladkikh@gmail.com>
2023-07-07 11:37:21 +0300

    [#185191055] Knlwoedgebase: Process DB constraint violations as ResourceConstraintViolationException in ProductSpecificationJpaService

--- knowledgebase/src/main/java/com/diac/awesomehardwaresupply/knowledgebase/service/ProductSpecificationJpaService.java
+++ knowledgebase/src/main/java/com/diac/awesomehardwaresupply/knowledgebase/service/ProductSpecificationJpaService.java
@@ -1,9 +1,12 @@
 package com.diac.awesomehardwaresupply.knowledgebase.service;
 
+import com.diac.awesomehardwaresupply.domain.exception.ResourceConstraintViolationException;
 import com.diac.awesomehardwaresupply.domain.exception.ResourceNotFoundException;
 import com.diac.awesomehardwaresupply.domain.model.ProductSpecification;
 import com.diac.awesomehardwaresupply.knowledgebase.repository.ProductSpecificationRepository;
+import jakarta.validation.ConstraintViolationException;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -69,10 +72,15 @@ public class ProductSpecificationJpaService implements ProductSpecificationServi
      *
      * @param productSpecification Новая спецификация товаров
      * @return Сохраненная спецификация товаров
+     * @throws ResourceConstraintViolationException в случае, если при обращении к ресурсу нарушаются наложенные на него ограничения
      */
     @Override
     public ProductSpecification add(ProductSpecification productSpecification) {
-        return productSpecificationRepository.save(productSpecification);
+        try {
+            return productSpecificationRepository.save(productSpecification);
+        } catch (DataIntegrityViolationException | ConstraintViolationException e) {
+            throw new ResourceConstraintViolationException(e.getMessage());
+        }
     }
 
     /**
@@ -81,24 +89,31 @@ public class ProductSpecificationJpaService implements ProductSpecificationServi
      * @param id                   Идентификатор спецификации товаров, данные которой необходимо обновить
      * @param productSpecification Объект с обновленными данными спецификации товаров
      * @return Обновленная спецификация товаров
+     * @throws ResourceNotFoundException при попытке обновить несуществующую спецификацию товаров
+     * @throws ResourceConstraintViolationException в случае, если при обращении к ресурсу нарушаются наложенные на него ограничения
      */
     @Override
     public ProductSpecification update(int id, ProductSpecification productSpecification) {
-        return productSpecificationRepository.findById(id)
-                .map(productSpecificationInDb -> {
-                    productSpecification.setId(id);
-                    return productSpecificationRepository.save(productSpecification);
-                }).orElseThrow(
-                        () -> new ResourceNotFoundException(
-                                String.format(PRODUCT_SPECIFICATION_DOES_NOT_EXIST_MESSAGE, id)
-                        )
-                );
+        try {
+            return productSpecificationRepository.findById(id)
+                    .map(productSpecificationInDb -> {
+                        productSpecification.setId(id);
+                        return productSpecificationRepository.save(productSpecification);
+                    }).orElseThrow(
+                            () -> new ResourceNotFoundException(
+                                    String.format(PRODUCT_SPECIFICATION_DOES_NOT_EXIST_MESSAGE, id)
+                            )
+                    );
+        } catch (DataIntegrityViolationException | ConstraintViolationException e) {
+            throw new ResourceConstraintViolationException(e.getMessage());
+        }
     }
 
     /**
      * Удалить спецификацию товаров из системы
      *
      * @param id Идентификатор спецификации товаров, которую необходимо удалить
+     * @throws ResourceNotFoundException при попытке удалить несуществующую спецификацию товаров
      */
     @Override
     public void delete(int id) {
